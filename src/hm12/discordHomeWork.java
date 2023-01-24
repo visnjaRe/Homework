561843dd4a0bd7c9277044daf04d87936dbf9897
visnjaRe <visnja.repac@gmail.com>
2023-01-24 14:41:59 -0600

    refactoring code

--- src/hm12/discordHomeWork.java
+++ src/hm12/discordHomeWork.java
@@ -4,23 +4,10 @@ public class discordHomeWork {
     public static void main(String[] args) {
         task1();
         task2();
-        
     }
-
-
-    }
-
-
-
-
-
-
-
-
-
     public static void task2() {
-        //Create a String and print it in reverse order (Sunday → yadnuS).
 
+        //Create a String and print it in reverse order (Sunday → yadnuS).
         String day="Sunday";
 
         for(int i=day.length()-1; i>=0; i--) {
@@ -34,16 +21,10 @@ public class discordHomeWork {
         for(int i=0; i<day.length(); i++) {
             System.out.print(day.charAt(day.length()-1-i));
         }
-
-
-
-
-
-
     }
 
     public static void task1() {
-        //        Create a String and if the String is not empty perform the following:
+//        Create a String and if the String is not empty perform the following:
 //        if the String has an odd number of characters and has 3 or more characters, print the character in the middle of the String.
 //        For Example String str=hello =>l
 
